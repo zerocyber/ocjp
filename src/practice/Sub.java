f1733bb5ed26c86850b538372d083d5c3722dbdb
zerocyber <zeroforever@naver.com>
2018-08-15 15:17:02 +0900

    0815

---
+++ src/practice/Sub.java
@@ -0,0 +1,18 @@
+package practice;
+
+class Base {
+	public static void main(String[] args) {
+		System.out.println("Base " + args[10]);
+	}
+}
+
+public class Sub extends Base {
+	public static void main(String[] args) {
+		System.out.println("Overriden " + args[1]);
+	}
+}
+
+/*And the commands:
+javac Sub.java
+java Sub 10 20 30
+What is the result? => Overriden 20 */
--- src/practice/Test2.java
+++ src/practice/Test2.java
@@ -6,13 +6,16 @@ public class Test2 {
 		int aw = 1, ay = 1;
 		try {
 			aw = ax % 2;
+			System.out.println("aw : " + aw);
 			ay = az / aw;
+			System.out.println("ay : " + ay);
 		} catch (ArithmeticException e1) {
 			System.out.println("Invalid Divisor");
 		} catch (Exception e2) {
 			aw = 1;
 			System.out.println("Divisor Changed");
 		}
+		System.out.println("ay, az, aw : " + ay + "," + az+ "," + aw);
 		ay = az / aw; // Line 14
 		System.out.println("Succesful Division " + ay);
 	}
