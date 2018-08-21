34a0af2cc22a32a17a3a09777150e0a47e052a4c
zerocyber <zeroforever@naver.com>
2018-08-21 17:35:29 +0900

    0821

---
+++ src/practice/TestOperator.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class TestOperator {
+	public static void main(String[] args) {
+		int result = 30 - 12 / (2 * 5) + 1;
+		System.out.print("Result = " + result);
+	}
+}
+
+//Result : 30    --> 수학 배웠으면 틀리면 개노답인듯...
