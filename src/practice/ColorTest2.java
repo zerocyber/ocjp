cded9d383021e1760f23ea1b687bd1e5eb31a333
zerocyber <zeroforever@naver.com>
2018-08-28 23:41:41 +0900

    0828

---
+++ src/practice/ColorTest2.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class ColorTest2 {
+
+	List colors = new ArrayList();
+	colors.add("green");
+	colors.add("red");
+	colors.add("blue");
+	colors.add("yellow");
+	colors.remove(2);
+	colors.add(3,"cyan");
+	System.out.print(colors);
+	
+}
