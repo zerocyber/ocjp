502e40d09f219cdfcc353dfd4f415e79b01c6177
zerocyber <zeroforever@naver.com>
2018-09-03 00:02:39 +0900

    0903

---
+++ src/practice/Test3.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Test3 {
+public static void main(String[] args) {
+int day = 1;
+switch (day) {
+case "7": System.out.print("Uranus");
+case "6": System.out.print("Saturn");
+case "1": System.out.print("Mercury");
+case "2": System.out.print("Venus");
+case "3": System.out.print("Earth");
+case "4": System.out.print("Mars");
+case "5": System.out.print("Jupiter");
+}
+}
+}
+
+//case 별로 있는 숫자들을 더블 쿼테이션을 빼고 숫자로 바꿔준다
